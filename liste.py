2a1d931958ff8a1fc96cc41ecc49c6205a27050a
İbrahim EDİZ <edizdesign@gmail.com>
2022-11-08 13:46:41 +0000

    08112022_3

--- liste.py
+++ liste.py
@@ -1,9 +1,10 @@
 liste = ["Abdullah","Anıl","Burak","Cem","Cihan","Emre","Dilay",
-"Eyuphan","Furkan","İsmail","Nafiz","Özge","Pelin","Sevda","Fırat","Ediz"]
+"Eyuphan","Furkan","İsmail","Nafiz","Özge","Pelin","Sevda","Fırat","Ediz","Serap"]
 import os
 dosyaismi = "Giris.py"
 for item in liste:
     if not os.path.exists(os.path.join("Egzersizler",item)):
         os.mkdir(os.path.join("Egzersizler",item))
     open(os.path.join("Egzersizler",item,dosyaismi),"a+")
+
 
