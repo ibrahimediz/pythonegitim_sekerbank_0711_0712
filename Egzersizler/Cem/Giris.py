fb407b22b68d196228782671a6e866655322c176
İbrahim EDİZ <edizdesign@gmail.com>
2022-11-08 14:15:00 +0000

    08112022_5

--- Egzersizler/Cem/Giris.py
+++ Egzersizler/Cem/Giris.py
@@ -1,5 +1,7 @@
 print("Öncelikle Kestane Balının Diyarı",1,2,3,4)
 
-isim = input("İsminiz:")
+##isim = input("İsminiz:")
+
+isim = 123 ##"Muhittin"
 
 print("merhaba",isim)
--- Egzersizler/Dilay/Giris.py
+++ Egzersizler/Dilay/Giris.py
@@ -1,4 +1,6 @@
-print("Merhaba",1,2,3,sep='-') 
- 
+#print fonksiyonnu
+print("Merhaba",1,2,3,sep='-',end='') 
+
+#input fonksiyonu
 isim=input("isminiz:") 
 print("Merhaba",isim)
