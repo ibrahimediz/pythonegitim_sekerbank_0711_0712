12cff9f1c23ef28cfc4e33e8bca2630c29db7493
İbrahim EDİZ <edizdesign@gmail.com>
2022-11-08 13:06:42 +0000

    08112022_1

---
+++ liste.py
@@ -0,0 +1,9 @@
+liste = ["Abdullah","Anıl","Burak","Cem","Cihan","Emre","Dilay",
+"Eyuphan","Furkan","İsmail","Nafiz","Özge","Pelin","Sevda","Fırat","Ediz"]
+import os
+dosyaismi = "ilk.py"
+for item in liste:
+    if not os.path.exists(os.path.join("Egzersizler",item)):
+        os.mkdir(os.path.join("Egzersizler",item))
+    open(os.path.join("Egzersizler",item,dosyaismi),"a+")
+
