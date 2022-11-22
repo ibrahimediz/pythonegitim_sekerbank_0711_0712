1c68fbc4d19dec764fcb549cb00b4074c7846bc2
İbrahim EDİZ <edizdesign@gmail.com>
2022-11-22 14:07:16 +0000

    22112022_2

--- liste.py
+++ liste.py
@@ -1,7 +1,7 @@
 liste = ["Abdullah","Anıl","Burak","Cem","Cihan","Emre","Dilay",
 "Eyuphan","Furkan","İsmail","Nafiz","Özge","Pelin","Sevda","Fırat","Ediz","Serap"]
 import os
-dosyaismi = "03_03_return_yield_Fonks.py"
+dosyaismi = "03_04_global_local_Fonks.py"
 for item in liste:
     if not os.path.exists(os.path.join("Egzersizler",item)):
         os.mkdir(os.path.join("Egzersizler",item))
