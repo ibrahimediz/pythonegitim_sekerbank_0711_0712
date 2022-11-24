0a28a961b3012090f9eb49e96d3f8bb586c4aa07
İbrahim EDİZ <edizdesign@gmail.com>
2022-11-24 12:45:30 +0000

    24112022_1

--- liste.py
+++ liste.py
@@ -1,7 +1,7 @@
 liste = ["Abdullah","Anıl","Burak","Cem","Cihan","Emre","Dilay",
 "Eyuphan","Furkan","İsmail","Nafiz","Özge","Pelin","Sevda","Fırat","Ediz","Serap"]
 import os
-dosyaismi = "03_05_lambda_recursion_Fonks.py"
+dosyaismi = "03_06_builtin_Fonk.py"
 for item in liste:
     if not os.path.exists(os.path.join("Egzersizler",item)):
         os.mkdir(os.path.join("Egzersizler",item))
