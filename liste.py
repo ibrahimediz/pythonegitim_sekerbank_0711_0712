e7890669a4a12ee9bb8044f4a1210d460b26ff30
İbrahim EDİZ <edizdesign@gmail.com>
2022-12-08 14:46:19 +0000

    08122022_1

--- liste.py
+++ liste.py
@@ -1,7 +1,7 @@
 liste = ["Abdullah","Anıl","Burak","Cem","Cihan","Emre","Dilay",
 "Eyuphan","Furkan","İsmail","Nafiz","Özge","Pelin","Sevda","Fırat","Ediz","Serap"]
 import os
-dosyaismi = "06_02_finally.py"
+dosyaismi = "07_01_Giris.ipynb"
 for item in liste:
     if not os.path.exists(os.path.join("Egzersizler",item)):
         os.mkdir(os.path.join("Egzersizler",item))
